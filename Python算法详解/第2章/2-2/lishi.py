0180238747319d7e3806f9838518e83035aefc5c
huoweikong <1154282938@qq.com>
2020-11-19 18:30:11 +0800

    11

--- Python算法详解/第2章/2-2/lishi.py
+++ Python算法详解/第2章/2-2/lishi.py
@@ -1,4 +1,4 @@
-from _collections import deque
+from _collections import deque              # 双向队列
 def search(lines, pattern, history=5):
     previous_lines = deque(maxlen=history)
 
@@ -9,11 +9,14 @@ def search(lines, pattern, history=5):
 # Example use on a file
 if __name__ == '__main__':
     with open('123.txt') as f:
-        for line, prevlines in search(f, 'python', 5):
+        for line, prevlines in search(f, 'python', 5):  # f 文本信息  ‘python'模式搜索  5 history
+            print("line: " , line)
+            print("prevlines: " , prevlines)   # prevlines是双向队列 deque()
             for pline in prevlines:
                 print(pline)  # print (pline, end='')
             print(line)  # print (pline, end='')
             print('-' * 20)
+            print("prevlines: ", prevlines)  # prevlines是双向队列 deque()
 q = deque(maxlen=3)
 q.append(1)
 q.append(2)
